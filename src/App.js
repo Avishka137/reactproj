0788311114056aebe42f8154d27cedc046d37d45
Avishka Vikum Hettige <avishkavikum731@gmail.com>
2025-12-15 00:18:34 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1> This is a react project</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -16,7 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> 
       </header>
     </div>
   );
