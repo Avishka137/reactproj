f78546e1dcf416a960dcde3d602e04aabe5fdc83
Avishka Vikum Hettige <avishkavikum731@gmail.com>
2025-12-15 21:55:03 +0530

    updated !

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import PartOne from './components/PartOne';
+import Home from './pages/home'; 
 
 function App() {
   return (
     <div className="App">
-      <h1> This is a react project</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-          <PartOne/>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> 
-      </header>
+      <Home/>  
     </div>
   );
 }
