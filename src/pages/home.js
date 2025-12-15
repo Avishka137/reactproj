4809549ff0c096d36a4a110e93c50ac0197ad831
Avishka Vikum Hettige <avishkavikum731@gmail.com>
2025-12-15 22:27:00 +0530

    updated !

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import TopicBox from '../components/TopicBox'
 
 export default function Home() {
   return (
     <div>
-        <h1> here the home page !</h1>
+        <TopicBox/>
     </div>
   )
 }
+ 
