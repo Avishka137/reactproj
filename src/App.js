8d5db7d59acad7f30b83a686b644a9c76bb1b763
Avishka Vikum Hettige <avishkavikum731@gmail.com>
2025-12-21 19:00:51 +0530

    updated !

--- src/App.js
+++ src/App.js
@@ -5,9 +5,20 @@ import Contact from './pages/contact';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
-  return (
+  let count = 0;
+
+  const increment = () => {
+    count += 1; 
+    console.log("Incremented:", count);
+  };
+
+  const decrement = () => {
+    count -= 1; 
+    console.log("Decremented:", count);
+  };  
+  return (  
+    <div>
     <Router>
-      <div>
         {/* Navigation Menu */}
         <nav style={{ padding: '20px', background: '#333', color: 'white' }}>
           <Link to="/" style={{ margin: '0 10px', color: 'white' }}>Home</Link>
@@ -21,8 +32,16 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </div>
+  
     </Router>
+  
+    <span>
+      My Counter
+    </span>
+    <p>Count is {count} </p>
+    <button onClick={increment}>+</button>
+    <button onClick={decrement}>-</button>
+    </div>
   );
 }
 
